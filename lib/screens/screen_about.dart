3371059a42b1aa8273bc07afcc6076a4203d6f0c
dschrubba <daniel.schrubba.91@gmail.com>
2025-07-24 03:30:18 +0200

    Added Sharky

--- lib/screens/screen_about.dart
+++ lib/screens/screen_about.dart
@@ -5,6 +5,34 @@ class ScreenAbout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    const double height = 120;
+    return Column(
+      children: [
+        SizedBox(
+          height: 60,
+        ),
+        Padding(
+          padding: EdgeInsetsGeometry.all(16),
+          child: SizedBox(
+            height: height,
+            width: double.maxFinite,
+            child: Center(
+              child: CircleAvatar(
+                radius: height / 2,
+                foregroundImage: Image.asset("assets/images/dummy_avatar.png").image,
+              )
+            ),
+          ),
+        ),
+        Text(
+          "Sharky Sharkson",
+          style: Theme.of(context).textTheme.headlineMedium,
+          ),
+        Text(
+          "The electrical ocean's finest UX/UI",
+          style: Theme.of(context).textTheme.bodyLarge,
+          ),
+      ],
+    );
   }
 }
